7d1942b2e8da632ad3f2b2ede17a29d7740f372e
Ryan S <planetlucid@gmail.com>
2019-08-24 23:01:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,12 +1,14 @@
 def sort_array_asc(numbers)
-numbers.sort 
+  numbers.sort 
 end
-
+#
+#
 def sort_array_desc(numbers)
-numbers.sort.reverse!
+  numbers.sort.reverse!
 end
+#
+#
+def sort_array_char_count(strings)
+  sort_array_char_count.sort 
 
-def sort_array_char_count(numbers)
-new_numbers=[]
-new_numbers << numbers.sort
 end
